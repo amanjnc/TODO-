6bc7007d9657996c879fe9f7fb6213126c4afaf2
amanuel <amanuelbeyne662@gmail.com>
2024-03-11 22:58:18 +0300

    finalized the front
    now about to componentize it

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,69 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-	const [newItem, setNewItem] = useState("");
-	const [todos, setTodos] = useState([]);
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
 
-	function handleSubmit(e) {
-		e.preventDefault();
-		setTodos([
-			...todos,
-			{ id: crypto.randomUUID(), title: newItem, completed: false },
-		]);
-		setNewItem(""); // Clear the newItem input field after submitting
-	}
+  function handleSubmit(e) {
+    e.preventDefault();
+     setTodos((todos) => [
+				...todos,
+				{ id: crypto.randomUUID(), title: newItem, completed: false },
+			]);
+    
+    
+    setNewItem("");
+  }
+  function togglechange(id, completed) {
+  setTodos((todos) =>
+    todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: completed };
+      }
+      return todo;
+    })
+  );
+}
+  function deleteTask(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
 
-	console.log(todos);
+  console.log(todos);
 
-	return (
-		<>
-			<form onSubmit={handleSubmit} className='new-item-form'>
-				<div className='form-row'>
-					<label htmlFor='item'>New item</label>
-					<input
-						value={newItem}
-						onChange={(e) => setNewItem(e.target.value)}
-						type='text'
-						id='item'
-					/>
-				</div>
-				<button className='btn'>ADD</button>
-			</form>
-			<h1 className='header'>TODO LIST</h1>
-			<ul className='list'>
-				{/* Render the todo items dynamically */}
-				{todos.map((todo) => (
-					<li key={todo.id}>
-						<label>
-							{todo.title}
-							<input type='checkbox' />
-						</label>
-						<button className='btn btn-danger'>Delete</button>
-					</li>
-				))}
-			</ul>
-		</>
-	);
+  return (
+    <>
+      <form onSubmit={(e) => handleSubmit(e) } className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">ADD</button>
+      </form>
+      <h1 className="header">TODO LIST </h1>
+      <ul className="list">
+        {todos.length ===0 && "No TOdos"}
+
+
+        {todos.map((todo) => (
+          <li >
+            <label>
+              {todo.title}
+              <input type="checkbox" checked={todo.completed} onChange={(e)=> togglechange(todo.id, e.target.checked)}   />
+            </label>
+            <button
+              onClick={() => deleteTask(todo.id)}
+              className="btn btn-danger"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
